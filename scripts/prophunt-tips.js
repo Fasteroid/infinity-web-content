2c025ebd08ada2b60de8271d551b4a6305b5dee3
Fasteroid <fasteroid@hotmail.com>
2022-06-05 20:56:56 -0400

    rules updates

--- scripts/prophunt-tips.js
+++ scripts/prophunt-tips.js
@@ -1,7 +1,7 @@
 const tips = [
     "As a hunter, spotting a prop awards you 50 points.",
     "Upon being spotted, props automatically play a 'fear' taunt.",
-    "Don't like spotting with F4? Bind <code>ph_spot</code> to something else!"
+    "Don't like spotting with Q? Bind <code>ph_spot</code> to something else!"
 ];
 
 const choice = Math.floor(Math.random()*tips.length);
