937f38d6114663644034fbdd8320b6121ee2cfd1
Fasteroid <fasteroid#hotmail.com>
2022-06-16 23:07:51 -0400

    more tips

--- scripts/prophunt-tips.js
+++ scripts/prophunt-tips.js
@@ -1,7 +1,11 @@
 const tips = [
-    "As a hunter, spotting a prop awards you 50 points.",
+    "As a hunter, spotting a prop awards you 20 points.",
     "Upon being spotted, props automatically play a 'fear' taunt.",
-    "Don't like spotting with Q? Bind <code>ph_spot</code> to something else!"
+    "Don't like spotting with Q? Bind <code>ph_spot</code> to something else!",
+    "As a prop, you can access the prop chooser with <code>F4.",
+    "Don't like using F4 for the prop menu? Bind <code>phe_access_propchoose</code> to something else.",
+    "The Broom pushes props in an area of effect and is good for sweeping rooms.",
+    "The Jump Pack increases your jump height and opens up new possibilities."
 ];
 
 const choice = Math.floor(Math.random()*tips.length);
